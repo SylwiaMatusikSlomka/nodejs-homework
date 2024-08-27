37971a82855daa527903d5352290ef39d7b264c0
Sylwia Matusik-Slomka <sylwia.matusik.1994@gmail.com>
2024-08-27 22:40:27 +0200

    added Mongodb implementation

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+const mongoose = require('mongoose');
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const url = "mongodb+srv://sylwia:ASlomka123@basecluster.4nqy1.mongodb.net/db-contacts";
+
+const server = async() => {
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connection successful");
+        app.listen(3000, () => {
+            console.log("Server is running localhost:3000");
+        });
+    } catch (err) {
+        console.error("MongoDB connection filled!!", err);
+        console.err(err), process.exit(1);
+    }
+};
+
+server();
