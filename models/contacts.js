ddea9e6e2b62056b5a43219e606db593bce61be7
Sylwia Matusik-Słomka <117019914+SylwiaMatusikSlomka@users.noreply.github.com>
2024-08-29 10:49:18 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -7,84 +7,86 @@ const contactsPath = "./models/contacts.json";
 const Contact = require("../service/schema/contact.js");
 
 const listContacts = () => {
-    return Contact.find()
-        .then((contacts) => {
-            return contacts;
-        })
-        .catch((error) => {
-            throw error;
-        });
+  return Contact.find()
+    .then((contacts) => {
+      return contacts;
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
 
 const getContactById = (contactId) => {
-    return Contact.find({ _id: contactId })
-        .then((contacts) => {
-            return contacts;
-        })
-        .catch((error) => {
-            throw error;
-        });
+  return Contact.find({ _id: contactId })
+    .then((contacts) => {
+      return contacts;
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
 
 const removeContact = (contactId) => {
-    return Contact.deleteOne({ _id: contactId })
-        .then((contacts) => {
-            return contacts;
-        })
-        .catch((error) => {
-            throw error;
-        });
+  return Contact.deleteOne({ _id: contactId })
+    .then((contacts) => {
+      return contacts;
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
 
 const addContact = (body) => {
-    return Contact.create({
-            name: body.name,
-            email: body.email,
-            phone: body.phone,
-            favorite: body.favorite
-        })
-        .then((contacts) => {
-            return contacts;
-        })
-        .catch((error) => {
-            throw error;
-        });
+  return Contact.create({
+      name: body.name,
+      email: body.email,
+      phone: body.phone,
+      favorite: false
+    })
+    .then((contacts) => {
+      return contacts;
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
 
 const updateContact = (contactId, body) => {
-    return Contact.updateOne({
-                _id: contactId
-            },
-            body
-        )
-        .then((contacts) => {
-            return contacts;
-        })
-        .catch((error) => {
-            throw error;
-        });
+  return Contact.updateOne(
+    { 
+      _id: contactId 
+    },
+    body
+  )
+  .then((contacts) => {
+    return contacts;
+  })
+  .catch((error) => {
+    throw error;
+  });
 };
 
 const updateStatusContact = (contactId, body) => {
-    console.log(contactId);
-    return Contact.updateOne({
-                _id: contactId
-            },
-            body
-        )
-        .then(() => {
-            return getContactById(contactId);
-        })
-        .catch((error) => {
-            throw error;
-        });
+  console.log(contactId);
+  return Contact.updateOne(
+    { 
+      _id: contactId 
+    },
+    body
+  )
+  .then(() => {
+    return getContactById(contactId);
+  })
+  .catch((error) => {
+    throw error;
+  });
 };
 
 module.exports = {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-    updateContact,
-    updateStatusContact
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  updateStatusContact
 }
